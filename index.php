de880da1275a767cb6123ca34f9e11451e354bbb
BSeeto <bianca_seeto@dialog.com.au>
2017-09-06 11:19:22 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,109 @@
+<?php 
+header('Content-Type: application/json');
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	
+	$companyValue = $json->result->parameters->companyName;
+	$searchValue = $json->result->parameters->searchType;
+	$officeLocationValue = $json->result->parameters->officeLocation;
+	$consultantNameValue = $json->result->parameters->consultantName;
+	$managerNameValue = $json->result->parameters->managerName;
+	
+	$linkAddr = "https://codex.dialoggroup.biz/?mode=voice";
+	
+	//&name=pete+ryan&company=dialog&office=south+brisbane&manager=alec+begley&search=.net-python-project+management
+	
+	// only process when consultant contains a value
+	if (!(is_null($consultantNameValue) || empty($consultantNameValue)) )
+	{
+		//if spaces, replace with +
+		if (preg_match('/\s/',$consultantNameValue)) 
+		{
+			$consultantNameValue = preg_replace('/\s+/', '+', $consultantNameValue);
+		}
+		$linkAddr .= "&name=" . $consultantNameValue;
+	}
+	
+	// only process when company contains a value
+	if (!(is_null($companyValue) || empty($companyValue)) )
+		
+	{
+		$linkAddr .= "&company=" . $companyValue;
+	}
+		
+	// only process when office location contains a value
+	if (!(is_null($officeLocationValue) || empty($officeLocationValue)) )
+	{
+		$linkAddr .= "&office=";
+		$newLocatValue = array();
+		foreach( $officeLocationValue as $value ) {
+		
+			//if spaces, replace with +
+			if (preg_match('/\s/',$value)) 
+			{
+				$newLocatValue[] = preg_replace('/\s+/', '+', $value);
+			}
+			else 
+			{
+				$newLocatValue[] = $value;
+			}
+		}
+		$linkAddr .= implode($newLocatValue, "-");
+	}
+	
+	// only process when manager contains a value
+	if (!(is_null($managerNameValue) || empty($managerNameValue)) )
+	{
+		//if spaces, replace with +
+		if (preg_match('/\s/',$managerNameValue)) 
+		{
+			$managerNameValue = preg_replace('/\s+/', '+', $managerNameValue);
+		}
+		$linkAddr .= "&manager=" . $managerNameValue;
+	}
+	
+	// only process when search contains a value
+	if (!(is_null($searchValue) || empty($searchValue)) )
+	{
+		$linkAddr .= "&search=";
+		$newSearchValue = array();
+		foreach( $searchValue as $value ) {
+		
+			//if spaces, replace with +
+			if (preg_match('/\s/',$value)) 
+			{
+				$newSearchValue[] = preg_replace('/\s+/', '+', $value);
+			}
+			else 
+			{
+					$newSearchValue[] = $value;
+			}
+		}
+		$linkAddr .= implode($newSearchValue, "-");
+	}
+	
+	//consume the link
+	$client = curl_init($linkAddr);
+	curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
+	
+	//get response from resource
+	$clientResponse = curl_exec($client);
+
+	$response = new \stdClass();
+	$response->speech = "This is your link.  Please log into Codex to find out the details of your search.";
+	$response->displayText = "This is your link. " . $linkAddr . ". Please log into Codex to find out the details of your search.";
+	$response->source = "Webhook for Dialog Codex";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+
+?>
