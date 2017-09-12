eb4fc5c50a4341b5c751259b1893f90d4f54140a
BSeeto <bianca_seeto@dialog.com.au>
2017-09-12 14:35:11 +1000

    testing again

--- index.php
+++ index.php
@@ -126,7 +126,7 @@ if($method == 'POST'){
 	  $response=array(
 			  "source" => "Webhook for Dialog Codex",
 			  "speech" => "This is your link: Please log into Dialog Codex to find out the details of your search.",
-			  "messages" => $messages,
+			  "data" => $messages,
 			  "contextOut" => array()
 		  );
 	
