e3440cc9956f90326242b2e01fbc4fd43c7064cc
BSeeto <bianca_seeto@dialog.com.au>
2017-09-14 09:27:05 +1000

    testing again

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ if($method == 'POST'){
 		"title"=> "Dialog Codex Search Link",
 		"subtitle"=> "Please log into Dialog Codex to find out the details of your search.",
 		"formattedText"=> "Please log into Dialog Codex to find out the details of your search.",
-		"image_url"=> "https://apaia-chatbot-webhook.herokuapp.com/app-logo.png",
+		"thumb_url"=> "https://apaia-chatbot-webhook.herokuapp.com/app-logo.png",
 		"buttons"=> [
 			[
 			  "text"=> "Dialog Codex Click Here",	
